0db021c1e4c08e325ead9d01ec0856f436649b2b
Danny Lin <danny@kdrag0n.dev>
2020-09-18 11:57:23 -0700

    wahoo: Enable compositor background blurring
    This allows us to create a "glass blur" look throughout the system for
    improved visuals.
    
    Change-Id: I77bb13a5711b05b43acf25e12abd9a2f5cd2502d

--- device.mk
+++ device.mk
@@ -719,6 +719,10 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.protected_contents=true
 PRODUCT_PRODUCT_PROPERTIES += \
     debug.hwui.renderer=skiavk
 
+# Enable blurs
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.surface_flinger.supports_background_blur=1
+
 # Use /product/etc/fstab.postinstall to mount system_other.
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.postinstall.fstab.prefix=/product
