a5ce5da519f545659ffa83ddd492637cbceefc0f
Lunarixus <Nathanb644@hotmail.co.uk>
2020-10-13 00:22:24 +0000

    wahoo: remove_packages: Remove more packages
    * With more source requires more space.
    
    Signed-off-by: Lunarixus <Nathanb644@hotmail.co.uk>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Chrome Chrome-Stub Maps
+LOCAL_OVERRIDES_PACKAGES := Chrome Chrome-Stub Maps Drive GoogleCamera GoogleTTS Music
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
