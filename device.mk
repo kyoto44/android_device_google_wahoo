5e706fb8db4a457ee68c9388be8b72b278d80ca8
Gibran Mather <39566883+gibranmather@users.noreply.github.com>
2020-11-04 20:28:59 +0000

    wahoo: Dexpreopt SystemUIGoogle

--- device.mk
+++ device.mk
@@ -692,7 +692,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Preopt SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += \
-    SystemUI
+    SystemUIGoogle
 
 # audio effects config
 PRODUCT_PROPERTY_OVERRIDES += \
