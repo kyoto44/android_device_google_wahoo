2af90d168bf7031545a25ddebea871568886a2c9
kubersharma001 <kubersharma001@gmail.com>
2021-08-17 17:37:41 +0000

    wahoo: Compile libldacBT_bco.so
    08-17 12:10:55.180 19022 19075 E a2dp_vendor_ldac_decoder: A2DP_VendorLoadDecoderLdac: cannot open LDAC decoder library libldacBT_bco.so: dlopen failed: library "libldacBT_bco.so" not found
    
    Change-Id: I910382a3affe8d02d6b0b20fd2dc3e91e57f3e9a

--- device.mk
+++ device.mk
@@ -355,6 +355,9 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-service-qti \
     android.hardware.bluetooth@1.0-service-qti.rc
 
+PRODUCT_PACKAGES += \
+    libldacBT_bco
+
 # Bluetooth SoC
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.qcom.bluetooth.soc=cherokee
