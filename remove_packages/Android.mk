581af545243d643f864edc15760dc0dc088e3243
PixelBoot <bootingpixels@gmail.com>
2021-10-25 09:27:09 -0700

    wahoo: remove packages to conserve system space

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := CalculatorGooglePrebuilt CalendarGooglePrebuilt CarrierMetrics CarrierWifi DevicePolicyPrebuilt DiagnosticsToolPrebuilt GoogleTTS Ornament Photos PrebuiltBugle PrebuiltDeskClockGoogle Showcase TipsPrebuilt Tycho talkback
+LOCAL_OVERRIDES_PACKAGES := CalculatorGooglePrebuilt CalendarGooglePrebuilt CarrierMetrics CarrierWifi CbrsNetworkMonitor DevicePolicyPrebuilt DiagnosticsToolPrebuilt GooglePrintRecommendationService GoogleTTS MarkupGoogle Ornament Photos PrebuiltBugle PrebuiltDeskClockGoogle SoundAmplifierPrebuilt Showcase TagGoogle TipsPrebuilt TurboPrebuilt Tycho talkback
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
