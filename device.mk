7b3a12f36d246bff5660a7e56ea02f5ecd0c9ec1
PixelBoot <bootingpixels@gmail.com>
2021-11-10 21:45:14 -0800

    wahoo: don't build bluetooth.audio@2.1

--- device.mk
+++ device.mk
@@ -536,7 +536,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl:32 \
     android.hardware.audio.effect@7.0-impl:32 \
     android.hardware.soundtrigger@2.2-impl:32 \
-    android.hardware.bluetooth.audio@2.1.vendor \
     android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.audio@2.0-service
 
